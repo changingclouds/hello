5fb2d4ce0b35dbbf33d6f7af7e6d37fd8119ab6a
changingclouds <changingclouds@126.com>
2016-06-13 23:16:04 +0800

    add

---
+++ qsort.js
@@ -0,0 +1,45 @@
+function quicksort( arr ){
+
+    var len = arr.length;
+
+    if( len <= 1  ){
+        return arr;
+    }else{
+        var pivotIndex = Math.floor( len / 2 );
+
+        var pivot = arr.splice( pivotIndex,1 )[0];
+
+        var left = [];
+        var right = [];
+        var element = null;
+
+        for (var i = 0; i < arr.length; i++) {
+            element = arr[i];
+            if( element < pivot  ){
+                left.push( element );
+            }else{
+                right.push( element );
+            }
+        }
+    }
+
+    console.log("pivot:"+pivot);
+    console.log( "left:"+left.join(",") );
+    console.log( "right:"+right.join(",") );
+
+    var result = quicksort( left ).concat( [pivot],quicksort( right )  ); 
+
+    console.log( "result:"+result );
+
+    console.log("-----------------------------------------");
+
+    return result;
+}
+
+var arr = [ 85, 24, 63, 45, 17, 31, 96, 50 ];
+
+console.log( arr );
+
+var result = quicksort( arr );
+
+console.log( result );
